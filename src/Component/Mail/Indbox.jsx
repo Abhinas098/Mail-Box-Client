c9ce84809a87884b1ccbe9ab256bfca6cff7b3fd
AbhinasDash <abhinasdash143@gmail.com>
2023-05-28 22:53:54 +0530

    delete message

--- src/Component/Mail/Indbox.jsx
+++ src/Component/Mail/Indbox.jsx
@@ -1,5 +1,5 @@
 import React, { useCallback, useEffect, useState } from "react";
-import { Card, ListGroup } from "react-bootstrap";
+import { Button, Card, ListGroup } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { emailActions } from "../../store/email-slice";
 import { Link } from "react-router-dom";
@@ -41,6 +41,23 @@ const Indbox = () => {
     }
   }, [mail, dispatch]);
 
+  const DeleteHandler = async (id) => {
+    console.log(id);
+    const res = await fetch(
+      `https://mail-box-client-710c7-default-rtdb.firebaseio.com/${mail}indbox/${id}.json`,
+      {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    let data = await res;
+    console.log(data);
+    GetData();
+  };
+
   useEffect(() => {
     GetData();
   }, [GetData]);
@@ -56,34 +73,46 @@ const Indbox = () => {
             data.length > 0 &&
             Object.keys(data).map((email, index) => {
               return (
-                <Link
+                <ListGroup.Item
                   key={index}
-                  style={{ textDecoration: "none" }}
-                  to={`/email/${data[email].id}`}
+                  className="bg-dark bg-gradient bg-opacity-50"
                 >
-                  {" "}
-                  <ListGroup.Item
-                    key={email.id}
+                  <Link
+                    key={index}
                     style={{
+                      float: "left",
+                      textDecoration: "none",
+                      color: "black",
                       cursor: "pointer",
-                      backgroundColor: "darkgray",
                     }}
+                    to={`/email/${data[email].id}`}
                   >
+                    {" "}
                     <span>
                       {data[email].read && (
-                        <h2 style={{ float: "right" }}>🟢</h2>
+                        <p className="mt-3 me-3 ms-0" style={{ float: "left" }}>
+                          🟢
+                        </p>
                       )}
                     </span>
                     <span>
-                      <b>From:</b> {data[email].from}
+                      <b>From:</b> {data[email].from.replace(/........$/, "")}
                     </span>
                     <br />
                     <span>
-                      <b>Subject: </b> {data[email].subject}
+                      <b>Subject: </b>
+                      {data[email].subject}
                     </span>
-                    <br />
-                  </ListGroup.Item>
-                </Link>
+                  </Link>
+                  <Button
+                    onClick={() => DeleteHandler(data[email].id)}
+                    key={data[email].id}
+                    style={{ float: "right" }}
+                    variant="danger"
+                  >
+                    Delete
+                  </Button>
+                </ListGroup.Item>
               );
             })}
         </ListGroup>
